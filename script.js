cf7edd4d7d3b9475a394fd8284052c2ee648d164
Valentin Caraba <luciancaraba@gmail.com>
2020-08-05 17:48:02 +0200

    Added some button functionality

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
+let rounds = 0;
+
 function computerPlay(){
     let sign = ['Rock', 'Paper', 'Scissors'];
     return sign[Math.floor(Math.random() * 10) % sign.length];
 }
 
-function playRound(playerSelection, computerSelection){
-    let player = playerSelection.toUpperCase();
-    let computer = computerSelection.toUpperCase();
-    if(player === 'ROCK'){
-        if(computer === 'SCISSORS'){
-            return 0;
-        }else if(computer === 'PAPER'){
-            return 1;
-        }else if(computer === 'ROCK'){
-            return 2; 
-        }
-    }else if(player === 'PAPER'){
-        if(computer === 'SCISSORS'){
-            return 1;
-        }else if(computer === 'PAPER'){
-            return 2;
-        }else if(computer === 'ROCK'){
-            return 0; 
-        }
-    }else if(player === 'SCISSORS'){
-        if(computer === 'SCISSORS'){
-            return 2;
-        }else if(computer === 'PAPER'){
-            return 0;
-        }else if(computer === 'ROCK'){
-            return 1; 
-        }
+function playRoundRock(){
+    let player = 'ROCK';
+    let computer = computerPlay().toUpperCase();
+    if(computer === 'SCISSORS'){
+        console.log('human');
+    }else if(computer === 'PAPER'){
+        console.log('computer');
+    }else if(computer === 'ROCK'){
+        console.log('draw'); 
     }
 }
+
+function playRoundPaper(){
+    let player = 'PAPER';
+    let computer = computerPlay().toUpperCase();
+    if(computer === 'SCISSORS'){
+        console.log('computer');
+    }else if(computer === 'PAPER'){
+        console.log('draw');
+    }else if(computer === 'ROCK'){
+        console.log('human'); 
+    }
+}
+
+function playRoundScissors(){
+    let player = 'SCISSORS';
+    let computer = computerPlay().toUpperCase();
+    if(computer === 'SCISSORS'){
+        console.log('draw');
+    }else if(computer === 'PAPER'){
+        console.log('human');
+    }else if(computer === 'ROCK'){
+        console.log('computer'); 
+    }
+}
+
+
+
+
 
 function game(){
     let playerWins = 0;
@@ -58,3 +70,15 @@ function game(){
         console.log(`Draw! ${playerWins} against ${computerWins}.`);
     }
 }
+
+const btnRock = document.querySelector('.rock');
+btnRock.addEventListener('click', playRoundRock);
+
+const btnPaper = document.querySelector('.paper');
+btnPaper.addEventListener('click', playRoundPaper);
+
+const btnScissors = document.querySelector('.scissors');
+btnScissors.addEventListener('click',playRoundScissors);
+
+
+
